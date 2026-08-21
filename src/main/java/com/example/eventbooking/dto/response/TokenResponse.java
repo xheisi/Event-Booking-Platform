ab60d41e84119a7b02f90a0ff55e0d3db7b31db1
xheisi <xheisihida483@gmail.com>
2026-08-21 21:02:40 +0200

    Filled in Token response with token token type and user

--- src/main/java/com/example/eventbooking/dto/response/TokenResponse.java
+++ src/main/java/com/example/eventbooking/dto/response/TokenResponse.java
@@ -1,4 +1,11 @@
 package com.example.eventbooking.dto.response;
+import lombok.*;
 
+@Builder
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 public class TokenResponse {
+    private String token;
+    private String tokenType;
+    private UserResponse user;
 }
