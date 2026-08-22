1c7ba352a310f5b90c82072c86e919dc320befaf
xheisi <xheisihida483@gmail.com>
2026-08-22 23:40:44 +0200

    Check from db if category and venue exist by name so we don't get dublicates

--- src/main/java/com/example/eventbooking/repository/VenueRepository.java
+++ src/main/java/com/example/eventbooking/repository/VenueRepository.java
@@ -4,4 +4,5 @@ import com.example.eventbooking.entity.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VenueRepository extends JpaRepository<Venue, Long> {
+    boolean existsByName(String name);
 }
